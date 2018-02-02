fe5799a95360993195112c73faf46fb4411abac4
deepesh KN <deepesh@pepo.com>
2018-02-02 15:21:11 +0530

    Tests: few changes

---
+++ test/services/pricer/constants.js
@@ -0,0 +1,25 @@
+const constants = {
+  deployer: "",
+  deployerPassphrase: "",
+  ops: "",
+  opsPassphrase: "",
+  account1: "",
+  accountPassphrase1: "",
+  account2: "",
+  accountPassphrase2: "",
+  account3: "",
+  accountPassphrase3: "",
+  account4: "",
+  accountPassphrase4: "",
+  currencyUSD: "USD",
+  currencyEUR: "EUR",
+  currencyINR: "INR",
+  currencyBlank: "",
+  priceOracleOstUsdAddress: "", //conversionRate is 5
+  priceOracleOstEurAddress: "", //conversionRate is 2
+  priceOracleOstUsdZeroPricePointAddress: "",
+  pricerOstAddress: "",
+  pricerOstZeroPricePointAddress: "",
+  pricerDummyAddress: "",
+}
+module.exports = constants;
--- test/services/pricer/get_pricepoint_and_calculated_amounts.js
+++ test/services/pricer/get_pricepoint_and_calculated_amounts.js
@@ -3,42 +3,58 @@ const assert = require('assert');
 
 // Load cache service
 const rootPrefix = "../../.."
+  , Utils = require(rootPrefix + './test/lib/utils.js');
+  , constants = require(rootPrefix + '/test/services/pricer/constants')
   , pricer = require(rootPrefix + '/lib/contract_interact/pricer')
-  , deployer = ""
-  , deployerPassphrase = ""
-  , ops = ""
-  , opsPassphrase = ""
-  , account1 =""
-  , accountPassphrase1 = ""
-  , account2 =""
-  , accountPassphrase2 = ""
-  , account3 =""
-  , accountPassphrase2 = ""
-  , priceOracleOstUsdAddress = ""
-  , priceOracleOstUsdZeroPricePointAddress = ""
+  , pricerOstUsd = new pricer(pricerOstAddress)
+  , pricerOstEur = new pricer(priceOracleOstEurAddress)
+  , pricerOstUsdZeroPricePoint = new pricer(pricerOstZeroPricePointAddress)
 ;
 
-describe('Pay', function() {
+describe('Get price point and calculated amounts', function() {
 
   it('should pass the initial address checks', async function() {
-    assert.notEqual(deployer, ops);
-    assert.notEqual(deployer, user);
-    assert.notEqual(ops, user);
+    assert.notEqual(constants.deployer, constants.ops);
+    assert.notEqual(constants.deployer, constants.user);
+    assert.notEqual(constants.ops, constants.user);
   });
 
   it('should fail when currency is 0', async function() {
-   
+    await Utils.expectThrow(
+      pricerOstUsd.getPricePointAndCalculatedAmounts(
+        constants.account1, 
+        constants.accountPassphrase1, 
+        toWei(1),
+        toWei(0.5),
+        currencyBlank)
+    );
   });
 
   it('should fail when price point is 0', async function() {
-   
+    await Utils.expectThrow(
+      pricerOstUsdZeroPricePoint.getPricePointAndCalculatedAmounts(
+        constants.account1, 
+        constants.accountPassphrase1, 
+        toWei(1),
+        toWei(0.5),
+        constants.currencyUSD)
+    );
   });
 
   it('should pass when all parameters are valid and conversion rate is 5', async function() {
-   
+    const amount = toWei(1)
+      , commissionAmount = toWei(0.5)
+      ;
+    var {pricePoint, tokenAmount, commissionTokenAmount } = await pricerOstUsd.getPricePointAndCalculatedAmounts(
+        constants.account1, 
+        constants.accountPassphrase1, 
+        amount,
+        commissionAmount,
+        constants.currencyUSD)
+    assert.equal(pricePoint > 0, true);
   });
 
-  it('should pass when all parameters are valid and conversion rate is 0.2', async function() {
+  it('should pass when all parameters are valid and conversion rate is 2', async function() {
    
   });
 
