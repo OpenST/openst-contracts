eb6c0c198af75b27b0f1a35c5998428d1859dc3f
kedarchandrayan <kedar@pepo.com>
2018-03-12 21:20:57 +0530

    notify dummy.

--- helpers/custom_console_logger.js
+++ helpers/custom_console_logger.js
@@ -12,62 +12,68 @@ const WIN_PRE = "\x1b[32m"; //Success (GREEN)
 const WARN_PRE = "\x1b[43m";
 const STEP_PRE = "\n\x1b[34m"; //Step Description (BLUE)
 
-
-// const CONSOLE_RESET = "";
-// const ERR_PRE = "";
-// const INFO_PRE = "";
-// const WIN_PRE = "";
-// const WARN_PRE = "";
-// const STEP_PRE = "";
-
-
 module.exports = {
-  "STEP_PRE": STEP_PRE
-  , "WARN_PRE": WARN_PRE
-  , "WIN_PRE": WIN_PRE
-  , "INFO_PRE": INFO_PRE
-  , "ERR_PRE": ERR_PRE
-  , "CONSOLE_RESET": CONSOLE_RESET
+  "STEP_PRE": STEP_PRE,
 
-  , step: function () {
+  "WARN_PRE": WARN_PRE,
+
+  "WIN_PRE": WIN_PRE,
+
+  "INFO_PRE": INFO_PRE,
+
+  "ERR_PRE": ERR_PRE,
+
+  "CONSOLE_RESET": CONSOLE_RESET,
+
+  step: function () {
     var args = [this.STEP_PRE];
     args = args.concat(Array.prototype.slice.call(arguments));
     args.push(this.CONSOLE_RESET);
     console.log.apply(console, args);
-  }
+  },
 
   //Method to Log Information
-  , info: function () {
+  info: function () {
     var args = [this.INFO_PRE];
     args = args.concat(Array.prototype.slice.call(arguments));
     args.push(this.CONSOLE_RESET);
     console.log.apply(console, args);
-  }
+  },
 
   //Method to Log Error.
-  , error: function () {
+  error: function () {
     var args = [this.ERR_PRE];
     args = args.concat(Array.prototype.slice.call(arguments));
     args.push(this.CONSOLE_RESET);
     console.log.apply(console, args);
-  }
+  },
 
-  , warn: function () {
+  warn: function () {
     var args = [this.WARN_PRE];
     args = args.concat(Array.prototype.slice.call(arguments));
     args.push(this.CONSOLE_RESET);
     console.log.apply(console, args);
-  }
+  },
 
   //Method to Log Success/Win.
-  , win: function () {
+  win: function () {
     var args = [this.WIN_PRE];
     args = args.concat(Array.prototype.slice.call(arguments));
     args.push(this.CONSOLE_RESET);
     console.log.apply(console, args);
-  }
+  },
 
-  , log: function () {
+  log: function () {
     console.log.apply(console, arguments);
+  },
+
+  /**
+   * Notify error - including the method so that code starts working.
+   */
+  notify: function () {
+    var args = [this.ERR_PRE];
+    args = args.concat(Array.prototype.slice.call(arguments));
+    args.push(this.CONSOLE_RESET);
+    console.log.apply(console, args);
   }
 };
