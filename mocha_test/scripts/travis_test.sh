bccdd6f9c3d82e8cc5b38036eb194b0fafc9a407
Sarvesh Jain <sarvesh@ost.com>
2018-05-18 15:57:14 +0530

    Running all mocha tests in one run so that summary tells the status of all tests

--- mocha_test/scripts/travis_test.sh
+++ mocha_test/scripts/travis_test.sh
@@ -3,7 +3,7 @@ sh start_test_chain.sh
 sh deploy_all.sh
 . ./env_vars.sh
 node ./../../migrations/create_tables.js
-./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/pricer  --exit
-./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/airdrop  --exit
+./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/pricer/*.js ./../../mocha_test/services/airdrop/*.js ./../../mocha_test/services/workers/*.js --exit
+#./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/airdrop  --exit
 # run worker test at last, as in the test case it removes the worker contract that is needed for other tests.
-./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/workers  --exit
+#./../../node_modules/mocha/bin/mocha  ./../../mocha_test/services/workers  --exit
