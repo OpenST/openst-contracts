85da3b0de5d8da69b990112867dc772cad4e4213
deepesh KN <deepesh@pepo.com>
2018-05-25 19:36:49 +0530

    Updated travis.yml to include build matrix

--- mocha_test/scripts/install_geth_1.8.8.sh
+++ mocha_test/scripts/install_geth_1.8.8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl https://gethstore.blob.core.windows.net/builds/geth-linux-arm64-1.8.8-2688dab4.tar.gz | tar xvz
-mv geth-linux-arm64-1.8.8-2688dab4 /usr/local/bin
-ln -s /usr/local/bin/geth-linux-arm64-1.8.8-2688dab4/geth /usr/local/bin/geth
-export PATH="$PATH:/usr/local/bin/geth-linux-arm64-1.8.8-2688dab4"
+curl https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.8-2688dab4.tar.gz | tar xvz
+mv geth-linux-amd64-1.8.8-2688dab4 /usr/local/bin
+ln -s /usr/local/bin/geth-linux-amd64-1.8.8-2688dab4/geth /usr/local/bin/geth
+export PATH="$PATH:/usr/local/bin/geth-linux-amd64-1.8.8-2688dab4"
 
 
 
