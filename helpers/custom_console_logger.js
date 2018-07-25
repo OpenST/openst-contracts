4e648b2c27ae125da3ef95f782aa321a3d136719
Dhananjay8 <dhananjay@ost.com>
2018-07-25 15:06:21 +0530

    Introduce storage and web3 provider factory

--- helpers/custom_console_logger.js
+++ helpers/custom_console_logger.js
@@ -8,9 +8,7 @@ const OSTBase = require('@openstfoundation/openst-base')
   , Logger  = OSTBase.Logger
 ;
 
-const rootPrefix = '..'
-  , coreConstants = require(rootPrefix + '/config/core_constants')
-  , loggerLevel = coreConstants.DEBUG_ENABLED == 1 ? Logger.LOG_LEVELS.TRACE : Logger.LOG_LEVELS.INFO
+const loggerLevel = process.env.OST_DEBUG_ENABLED == '1' ? Logger.LOG_LEVELS.TRACE : Logger.LOG_LEVELS.INFO
 ;
 
 module.exports = new Logger("openst-payments", loggerLevel);
