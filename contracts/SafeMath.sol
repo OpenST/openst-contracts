840797de1895bc74f4de43613d1b54540515e2ed
Gulshan Vasnani <gulshan@ost.com>
2018-10-17 12:03:56 +0530

    Styling changes

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -38,7 +38,7 @@ library SafeMath {
      */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         // Gas optimization: this is cheaper than requiring 'a' not being zero,
-        //  but the benefit is lost if 'b' is also tested.
+        // but the benefit is lost if 'b' is also tested.
         // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
         if (a == 0) {
             return 0;
@@ -58,8 +58,9 @@ library SafeMath {
         // Solidity only automatically asserts when dividing by 0.
         require(b > 0);
         uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this
-                                       // doesn't hold
+
+        // There is no case in which this doesn't hold
+        // assert(a == b * c + a % b);
 
         return c;
     }
