e3227ea4780fe2422cf5c70148871aee922f97d0
Paruyr Gevorgyan <paruyr@ost.com>
2019-04-04 11:21:12 +0200

    trailofbits/eth-security-toolbox docker is pulled right before using slither.

--- tools/docker_run_slither.sh
+++ tools/docker_run_slither.sh
@@ -3,6 +3,8 @@
 script_dir_path="$(cd "$(dirname "$0")" && pwd)"
 root_dir="${script_dir_path}/.."
 
+docker pull trailofbits/eth-security-toolbox || exit 1
+
 container=$(docker run -it -d -v "${root_dir}":/share trailofbits/eth-security-toolbox) || exit 1
 
 docker exec -it "${container}" bash -c \
@@ -15,4 +17,4 @@ slither_result=$?
 
 docker kill "${container}" || exit 1
 
-exit $slither_result
+exit ${slither_result}
